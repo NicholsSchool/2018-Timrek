50a9de617dad203f175ffb69ec8b619e1618f866
Anuraag-Kunar <ssbn0m4d@gmail.com>
2018-01-27 13:16:06 -0500

    Added Outtake

---
+++ src/org/usfirst/frc4930/Timrek/commands/Outtake.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc4930.Timrek.commands;
+
+import org.usfirst.frc4930.Timrek.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Outtake extends Command {
+	
+		protected void initialize() {
+
+		}
+		
+		protected void execute() {
+			Robot.gripper.grip(-0.6);
+		}
+		
+		@Override
+		protected boolean isFinished() {
+			
+			return false;
+		}
+		
+		protected void end() {
+			Robot.gripper.stop();
+		}
+		
+		protected void interrupted() {
+			end();
+		}
+
+	}
+
+
+
