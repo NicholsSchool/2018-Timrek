62b3e68b3638df5aa70ff990c36e557fac9ad720
Arnav Parashar <Arnav@arnavs-air.nicholsschool.org>
2018-02-08 16:57:14 -0500

    added gripper code

---
+++ src/org/usfirst/frc4930/Timrek/subsystems/Gripper.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc4930.Timrek.subsystems;
+
+import org.usfirst.frc4930.Timrek.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Gripper extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void grip(double speed) {
+		RobotMap.lIntake.set(speed);
+		RobotMap.rIntake.set(speed);
+	}
+	
+	public void stop() {
+		RobotMap.lIntake.stopMotor();
+		RobotMap.rIntake.stopMotor();
+	}
+}
