57891b57f7b9fadef91fff4c656ed3782d902ea9
Sreerama Jupudy <sreeramajupudy@Sreeramas-MBP.fios-router.home>
2018-03-01 21:14:11 -0500

    changed some PTO values

--- src/org/usfirst/frc4930/Timrek/subsystems/PTO.java
+++ src/org/usfirst/frc4930/Timrek/subsystems/PTO.java
@@ -5,23 +5,24 @@ import org.usfirst.frc4930.Timrek.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class PTO extends Subsystem {
+public class PTO extends Subsystem
+{
 
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public void turnOff() {
-		RobotMap.solenoid1.set(true);
-		Robot.ptoOn = false;
-		
-	}
-	
-	public void turnOn() {
-		RobotMap.solenoid1.set(false);
-		Robot.ptoOn = true;
-	}
+  @Override
+  protected void initDefaultCommand() {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void turnOff() {
+    RobotMap.solenoid1.set(false);
+    Robot.ptoOn = true;
+
+  }
+
+  public void turnOn() {
+    RobotMap.solenoid1.set(true);
+    Robot.ptoOn = false;
+  }
 
 }
